60142eb9fb255d5211faae20d1cf66db917342fb
Eitan Suez <esuez@pivotal.io>
2020-03-12 13:27:33 -0500

    removing nat router and nat address (don't think they're required for concourse)

--- network.tf
+++ network.tf
@@ -39,17 +39,3 @@ resource "google_compute_firewall" "internal" {
   ]
 }
 
-resource "google_compute_address" "nat-address" {
-  name   = "${var.environment_name}-cloud-nat"
-  region = var.region
-}
-
-resource "google_compute_router" "nat-router" {
-  name    = "${var.environment_name}-nat-router"
-  region  = var.region
-  network = google_compute_network.network.self_link
-
-  bgp {
-    asn = 64514
-  }
-}
